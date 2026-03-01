b1320f5cb9b0e78a99b4e99051a944ec0d18e739
BleytMC <150685954+BleytMC@users.noreply.github.com>
2026-03-01 12:54:33 +0200

    4

---
+++ 006_HW1/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _006_HW1
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "DATA SOURCE=RomanPC; DATABASE=EFCarsDB; UID=sa; PWD=1; TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+        }
+    }
+}
---
+++ 006_HW1/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace _006_HW1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Color { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
